33de8a057080fec0792343d694b08008b07c6aec
Shun Chu <shun.chu@gmail.com>
2017-08-17 16:39:18 -0700

    add credit for Noodler

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <div class="row">
-      <div class="col-sm-12"><h1>Noodler's Ink Selector</h1></div>
+      <div class="col-sm-12">
+        <h1>Noodler's Ink Selector</h1>
+        <span><span class="font-weight-bold">Credit:</span> Data used here are provided by <a href="http://noodlersink.com/noodlers-ink-properties/" target="_blank">Noodler's Ink Properties spreadsheet</a>.</span>
+      </div>
       <ink-filters class="col-sm-2"></ink-filters>
       <inks-table class="col-sm-10"></inks-table>
     </div>
