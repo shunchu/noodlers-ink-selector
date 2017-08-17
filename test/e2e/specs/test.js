77a66d94bc23c1ddb293fbf617fbe7f7843c8abc
Shun Chu <shun.chu@gmail.com>
2017-08-17 16:39:55 -0700

    fix tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,9 +11,10 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.filters')
+      .assert.elementPresent('select')
+      .assert.elementPresent('h1', 1)
+      .assert.elementPresent('table')
       .end()
   }
 }
--- test/unit/specs/InkFilters.spec.js
+++ test/unit/specs/InkFilters.spec.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
 import InkFilters from '@/components/InkFilters'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const mockStore = {
+  state: {
+    inks: [],
+    filters: [],
+    filteredInks: [],
+    selectedColor: undefined
+  }
+}
+
+const Constructor = Vue.extend(InkFilters)
+const vm = new Constructor({store: mockStore}).$mount()
 
 describe('InkFilters.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(InkFilters)
-    const vm = new Constructor().$mount()
-
     expect(
       vm.$el.querySelector('.filters h4').textContent
     ).to.equal('Filters')
