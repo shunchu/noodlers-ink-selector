a567d12567f35b7e95f4573a49e17d17304c3105
Shun Chu <shun.chu@gmail.com>
2017-08-17 17:42:06 -0700

    change font size

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-sm-12">
         <h1>Noodler's Ink Selector</h1>
-        <span><span class="font-weight-bold">Credit:</span> Data used here are provided by <a href="http://noodlersink.com/noodlers-ink-properties/" target="_blank">Noodler's Ink Properties spreadsheet</a>.</span>
+        <span class="credit"><span class="font-weight-bold">Credit:</span> Data used here are provided by <a href="http://noodlersink.com/noodlers-ink-properties/" target="_blank">Noodler's Ink Properties spreadsheet</a>.</span>
       </div>
       <ink-filters class="col-sm-2"></ink-filters>
       <inks-table class="col-sm-10"></inks-table>
@@ -39,5 +39,9 @@ export default {
     color: #2c3e50;
     margin-top: 60px;
     font-size: 1rem;
+  }
+
+  .credit {
+    font-size: 0.85rem;
   }
 </style>
