201371658fb0cdddfe07ec3f01db681504a9a85f
Shun Chu <shun.chu@gmail.com>
2017-08-21 13:53:12 -0700

    add tests

--- test/unit/specs/InkFilters.spec.js
+++ test/unit/specs/InkFilters.spec.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import InkFilters from '@/components/InkFilters'
+import 'babel-polyfill'
 import Vuex from 'vuex'
+import sinon from 'sinon'
+import chai from 'chai'
+import sinonChai from 'sinon-chai'
+
+chai.use(sinonChai)
 
 Vue.use(Vuex)
 
@@ -10,19 +16,34 @@ const inks = [
   { name: 'ink3', stockNo: '45678', colors: ['blue', 'black'], uvResistant: 'partial', archival: 'partial', tamperProof: true, waterproof: true, fluorescent: false, lubricated: false, freezeResistant: false, exclusive: false, notes: '' }
 ]
 
-const mockStore = {
+const mockStore = new Vuex.Store({
   state: {
     inks: inks,
     filters: [],
     filteredInks: [],
     selectedColor: undefined
+  },
+
+  actions: {
+    filterInks: sinon.stub(),
+    selectColor: sinon.stub()
+  },
+
+  mutations: {
+    filterInks: sinon.stub(),
+    selectColor: sinon.stub()
   }
-}
+})
 
 const Constructor = Vue.extend(InkFilters)
-const vm = new Constructor({store: mockStore}).$mount()
 
 describe('InkFilters.vue', () => {
+  let vm
+
+  beforeEach(() => {
+    vm = new Constructor({ store: mockStore }).$mount()
+  })
+
   it('should render correct contents', () => {
     expect(
       vm.$el.querySelector('.filters h4').textContent
@@ -36,4 +57,28 @@ describe('InkFilters.vue', () => {
       vm.$el.querySelectorAll('.filters select.colors option').length
     ).to.equal(4)
   })
+})
+
+describe('template actions', () => {
+  let vm
+
+  beforeEach(() => {
+    vm = new Constructor({ store: mockStore })
+  })
+
+  it('should respond to filter checkboxes', () => {
+    sinon.spy(vm, 'updateInks')
+    vm.$mount()
+
+    const archivalCheckbox = vm.$el.querySelector('.filters ul li input[value="archival"]')
+    const clickEvent = new window.Event('click')
+
+    archivalCheckbox.dispatchEvent(clickEvent)
+    vm._watcher.run()
+
+    sinon.assert.calledOnce(vm.updateInks)
+  })
+
+  // haven't figured out how to test in unit tests yet
+  xit('should respond to color selector', () => {})
 })
