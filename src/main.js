114efa57fbaed97e950d314f1eefedfbe38a076f
Shun Chu <shun.chu@gmail.com>
2025-04-12 12:13:07 -0700

    replace Bootstrap framework and icons with Tailwind and Fontawesome icons

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createBootstrap } from 'bootstrap-vue-next'
 import App from './App.vue'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import './assets/icons.css'
-
-const pinia = createPinia()
-const bootstrap = createBootstrap()
+import '@fortawesome/fontawesome-free/css/all.css'
+import './assets/main.css'
 
 const app = createApp(App)
-app.use(pinia)
-app.use(bootstrap)
+app.use(createPinia())
 app.mount('#app')
